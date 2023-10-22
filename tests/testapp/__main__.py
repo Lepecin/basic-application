9a78591ed74792c5e8837a58175984faf6687da8
Lepecin <evantrulife@gmail.com>
2023-10-22 20:01:46 +0100

    Add dummy command for saving

--- tests/testapp/__main__.py
+++ tests/testapp/__main__.py
@@ -3,15 +3,28 @@ from lepapplication.commands.test import (
     TestOneCommand,
     TestTwoCommand,
     TestThreeCommand,
+    Command,
 )
+
+
+class SaveConfigCommand(Command):
+    def command(self, **data) -> list[str]:
+        data_saved = {
+            "greeting": "hello",
+            "farewell": "bye",
+            **data,
+        }
+        print(f"Saved data {data_saved}")
+        return []
 
 
 app = App(
     TestOneCommand("one"),
     TestTwoCommand("two"),
     TestThreeCommand("three"),
+    SaveConfigCommand("save"),
 )
-app.get_metadata()
+app.get_metadata(**{})
 
 
 option, data = get_args()
