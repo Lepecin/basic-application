2d87aee9ee59483a91764388d8cb0d342fce5601
Lepecin <evantrulife@gmail.com>
2023-10-22 11:50:31 +0100

    Create compoound command

--- src/lepapplication/commands/abstract.py
+++ src/lepapplication/commands/abstract.py
@@ -13,3 +13,19 @@ class Command(__CommandAbstract):
     def __init__(self, name: str) -> None:
         super().__init__()
         self.name = name
+
+
+class CompoundCommand(Command):
+    commands: list[Command]
+
+    def __call__(self, *commands: Command) -> "CompoundCommand":
+        self.commands = list(commands)
+        return self
+
+    def command(self, **data) -> list[str]:
+        args: list[str] = []
+        for command in self.commands:
+            args.append(";")
+            args.extend(command.command(**data))
+
+        return args
