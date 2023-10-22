0ab62ee902d78d171918f617385dc26de187b1a7
Lepecin <evantrulife@gmail.com>
2023-10-22 12:24:17 +0100

    Add test commands

--- src/lepapplication/commands/__init__.py
+++ src/lepapplication/commands/__init__.py
@@ -1,1 +1,6 @@
 from .abstract import Command
+from .test import (
+    TestOneCommand,
+    TestTwoCommand,
+    TestThreeCommand,
+)
---
+++ src/lepapplication/commands/test.py
@@ -0,0 +1,27 @@
+from .abstract import Command, CompoundCommand
+
+
+class TestOneCommand(Command):
+    def command(self, message: str, **data) -> list[str]:
+        print(f"Running command: {self.__class__.__name__}")
+        print(f"This command prints message one: {message}")
+        print(f"Additional arguments include:")
+        for key, value in data.items():
+            print(key, value)
+        return []
+
+
+class TestTwoCommand(Command):
+    def command(self, **data) -> list[str]:
+        print(f"Running command: {self.__class__.__name__}")
+        print("A dull command :)")
+        return []
+
+
+class TestThreeCommand(CompoundCommand):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        super().__call__(
+            TestOneCommand(),
+            TestTwoCommand(),
+        )
