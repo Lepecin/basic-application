4af519de46bfb8d4bf9060d65a5d6b8d77209bfa
Lepecin <evantrulife@gmail.com>
2023-10-23 19:48:58 +0100

    Add imports

--- src/lepapplication/__init__.py
+++ src/lepapplication/__init__.py
@@ -1,6 +1,10 @@
 from .application import App
+from .arguments import get_args
 from .commands import (
     Command,
     CompoundCommand,
+    InterfaceCommand,
+    args_to_script,
+    data_to_args,
+    Script,
 )
-from .arguments import get_args
--- src/lepapplication/commands/__init__.py
+++ src/lepapplication/commands/__init__.py
@@ -1,4 +1,4 @@
-from .abstract import (
-    Command,
-    CompoundCommand,
-)
+from .abstract import Command, CompoundCommand
+from .cmd import args_to_script, Script
+from .interface import InterfaceCommand
+from .keyconvert import data_to_args
